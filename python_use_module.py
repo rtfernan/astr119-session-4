4f92463fc9e1d2ac9e687b27fa12149661d60295
rtfernan <43651617+rtfernan@users.noreply.github.com>
2018-10-09 11:26:26 -0700

    Uploaded Files
    dictionaries
    exceptions
    test module
    use module
    useful modules
    numpy

---
+++ python_use_module.py
@@ -0,0 +1,3 @@
+import test_module as tm	#calls another python program
+
+tm.hello_world()
---
+++ useful-modules.py
@@ -0,0 +1,14 @@
+import numpy as np					#the Numpy library
+import matplotlib.pyplot as plt		#Matplotlib's pyplot
+										#makes any kind of plot i.e.line, histogram, etc.
+						
+
+import sys					#gives access to a C-like sys library
+									#access command line arguments, change 
+									#functionality w/o changing program
+import os					#gives access to operating system
+									#prints as a list of strings
+
+print(sys.argv)		#prints any command line arguments, incl program name
+print(os.getcwd())	#prints the current working directory
+
